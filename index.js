ff226c89177a5e6a0a2b16b79b9b0172b5bd277e
ddeleon267 <dominique.a.deleon@gmail.com>
2021-08-20 16:57:59 -0400

    commit tree a47e005256a8aaf5fdd1fcf036c5bcc2c7df4f95

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+// const record = [
+//   { year: "2018", result: "L" },
+//   { year: "2017", result: "W" },
+//   { year: "2016", result: "N/A" }
+//   //...
+// ]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(win) {
+  let winningObj = win.find(item => item.result === 'W')
+  return winningObj ? winningObj.year : undefined
 }
+console.log(record.find(superbowlWin))
